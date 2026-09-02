551d591d85a5f5bc35bb165eb9b7ddcf410c37e6
Jagan Bharath <jaganbharath46@gmail.com>
2026-09-02 16:14:15 +0530

    Add SECURE_PROXY_SSL_HEADER to fix CSRF failures behind Render's proxy

--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,5 @@ SMS_API_KEY = os.environ.get("SMS_API_KEY", "")
 SMS_SENDER_ID = os.environ.get("SMS_SENDER_ID", "")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = ["https://payroll-ixgj.onrender.com"]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
