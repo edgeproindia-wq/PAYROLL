4a2819a33dfebf04967725077c64223ec8f246ee
Jagan Bharath <jaganbharath46@gmail.com>
2026-09-02 14:55:12 +0530

    Add reset_admin management command for one-time password reset on Render

---
+++ payroll_app/management/commands/reset_admin.py
@@ -0,0 +1,11 @@
+﻿from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        user, created = User.objects.get_or_create(username="admin", defaults={"is_staff": True, "is_superuser": True})
+        user.is_staff = True
+        user.is_superuser = True
+        user.set_password("EdgePro@2026")
+        user.save()
+        self.stdout.write("Admin password reset successfully.")
